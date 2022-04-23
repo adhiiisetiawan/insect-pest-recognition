d0403c63f7da66b10e21024efa2639c8c800e063
adhiiisetiawan <adhisetiawan518@gmail.com>
2022-04-23 07:15:59 +0000

    generate dataset

--- src/utils/download_data.py
+++
@@ -1,6 +0,0 @@
-import gdown
-
-# https://drive.google.com/file/d/1VRBiCVJvCUoFImkUWjKWYfPCCi3Vl42L/view?usp=sharing
-id = "1VRBiCVJvCUoFImkUWjKWYfPCCi3Vl42L"
-output = "../../data/ip102.tar"
-gdown.download(id=id, output=output, quiet=False)
