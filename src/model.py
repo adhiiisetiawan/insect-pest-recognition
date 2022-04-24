5718ad8fce92d2ec339e0064d78fff843eac4456
adhiiisetiawan <adhisetiawan518@gmail.com>
2022-04-24 08:02:53 +0000

    implement mobilenetv3 architecture

---
+++ src/model.py
@@ -0,0 +1,34 @@
+from torch import nn
+from torchvision.models import mobilenet_v3_small
+
+class InsectPestClassifier(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.mobilenet = mobilenet_v3_small(pretrained=True)
+        self.freeze()
+        self.mobilenet.classifier = nn.Sequential(
+            nn.Linear(1280, 1024),
+            nn.ReLU(),
+            nn.Dropout(0.2),
+
+            nn.Linear(1024, 512),
+            nn.ReLU(),
+            nn.Dropout(0.2),
+
+            nn.Linear(512, 256),
+            nn.ReLU(),
+            nn.Dropout(0.2),
+
+            nn.Linear(256, 102)
+        )
+        
+    def forward(self, x):
+        return self.mobilenet(x)
+    
+    def freeze(self):
+        for param in self.mobilenet.parameters():
+            param.requires_grad = False
+        
+    def unfreeze(self):
+        for param in self.mobilenet.parameters():
+            param.requires_grad = True
