4db4902479df97f167044d28a5bac15e8e0ff804
adhiiisetiawan <adhisetiawan518@gmail.com>
2022-04-26 01:12:38 +0000

    implement fine tuning

--- train.py
+++ train.py
@@ -25,10 +25,10 @@ model_children = list(model.children())
 summary(model, (3, 224, 224))
 
 
-wandb.init(project='insect-pest-recognition', name="MobileNetV3-small_cutmix_sparse_dlr", resume=True, reinit=True)
+wandb.init(project='large-scale-pest-recognition', name="MobileNetV3-small_cutmix_sparse_dlr", resume=True, reinit=True)
 wandb.watch(model, log='all')
 
-epochs = 200
+epochs = 150
 train_cost, val_cost = [], []
 train_acc, val_acc = [], []
 for i in range(epochs):
@@ -50,3 +50,33 @@ torch.save({
     "optimizer_state_dict": optimizer.state_dict(),
     "scheduler":scheduler.state_dict(),
 }, "./output/checkpoint1.pth")
+
+
+# Fine Tuning
+model.unfreeze()
+optimizer = optim.AdamW(model.parameters(), lr=0.0001)
+scheduler = StepLR(optimizer, step_size=50, gamma=0.1)
+
+summary(model, (3, 224, 224))
+
+ft_epochs = 150
+train_cost, val_cost = [], []
+train_acc, val_acc = [], []
+for i in range(ft_epochs):
+    cost_train, acc_train = loop_function('train', train_dl.dataset, train_dl, model, criterion, optimizer, device, model_children)
+    train_cost.append(cost_train)
+    train_acc.append(acc_train)
+    with torch.no_grad():
+        cost_val, acc_val = loop_function('val', val_dl.dataset, val_dl, model, criterion, optimizer, device, model_children)
+        val_cost.append(cost_val)
+        val_acc.append(acc_val)
+    scheduler.step()
+      
+    wandb.log({"train_cost":train_cost[-1], "val_cost":val_cost[-1], "train_acc": train_acc[-1], "val_acc":val_acc[-1]})
+    print(f"\rEpoch: {i+1}/{epochs} | train_cost: {train_cost[-1]:.4f} | val_cost: {val_cost[-1]:.4f} | "
+          f"train_acc: {train_acc[-1]:.4f} | val_acc: {val_acc[-1]:.4f} | Learning rate: {scheduler.get_last_lr()}")
+
+print("Finished training")
+wandb.finish()
+
+torch.save(model.state_dict(), "./output/mobilenetv3_cutmix_sparse_dlr.pth")
